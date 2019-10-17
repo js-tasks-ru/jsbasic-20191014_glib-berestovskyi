a80503b14a6d60956ea46c40ba59a139c356f049
Glib Berestovskyi <gberestovskyi@markettraders.com>
2019-10-17 16:47:29 +0300

    Sum function added.

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,6 +4,12 @@
  * @param {number} n index
  * @returns {number}
  */
+let m = 1;
+let n = 1;
+let p = 2;
+let q = 7;
 function sum(m, n) {
-
+  return (m + n);
 }
+alert(sum(m, n));
+alert(sum(p, q));
